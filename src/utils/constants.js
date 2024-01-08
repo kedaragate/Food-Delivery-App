98471564fc8e13f2ecd21400a4bc473ba9728084
kedaragate <100003176+kedaragate@users.noreply.github.com>
2024-01-08 12:13:34 +0530

    Update constants.js
    Proxy url added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@ export const IMG_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 const latLang = "lat=18.524545&lng=73.6981481";
-export const RESTAURANT_DATA_URL = `https://www.swiggy.com/dapi/restaurants/list/v5?${latLang}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
-
+export const RESTAURANT_DATA_URL = "https://api.allorigins.win/get?url=${encodeURIComponent"(`https://www.swiggy.com/dapi/restaurants/list/v5?${latLang}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
+)}"
 export const RESTAURANTS_MENU_URL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&${latLang}&restaurantId=`;
