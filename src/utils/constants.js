005400460b2f8aa7e3d1221d660d4195b638dec0
kedaragate <100003176+kedaragate@users.noreply.github.com>
2024-01-08 12:29:43 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ export const IMG_URL =
 
 const latLang = "lat=18.524545&lng=73.6981481";
  const actualResDataUrl=`https://www.swiggy.com/dapi/restaurants/list/v5?${latLang}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
-export const RESTAURANT_DATA_URL = `https://api.allorigins.win/get?url=${actualResDataUrl}`;
+export const RESTAURANT_DATA_URL =`https://api.allorigins.win/get?url=${actualResDataUrl}`;
 
 export const RESTAURANTS_MENU_URL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&${latLang}&restaurantId=`;
